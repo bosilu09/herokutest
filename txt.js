8a25fa54fa42917b0bffc42efd0d443a886d65c0
bosilu09 <84651759+bosilu09@users.noreply.github.com>
2023-12-27 15:18:12 +0000

    pupperteer

--- txt.js
+++ txt.js
@@ -1,3 +1,30 @@
-function hello (){ console.log("Program is Running")
-hello()}
-hello()
+const puppeteer = require('puppeteer');
+
+async function getSourceCode(url) {
+  // Launch a headless browser
+  const browser = await puppeteer.launch();
+
+  // Open a new page
+  const page = await browser.newPage();
+
+  // Navigate to the provided URL
+  await page.goto(url);
+
+  // Get the source code of the page
+  const sourceCode = await page.content();
+
+  // Close the browser
+  await browser.close();
+
+  return sourceCode;
+}
+
+// Example usage
+const url = 'https://example.com';
+getSourceCode(url)
+  .then(sourceCode => {
+    console.log('Source Code:', sourceCode);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
